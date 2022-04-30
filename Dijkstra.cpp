a459d7db880c4c4e9498df95ae41dee4fd47a935
M Shashanka <56446258+Shaky-Adino@users.noreply.github.com>
2022-05-01 01:21:02 +0530

    Add files via upload

---
+++ Dijkstra.cpp
@@ -0,0 +1,44 @@
+//for weighted graph
+
+vi dis;
+
+void dijkstra(int s, int n)
+{
+    dis.assign(n, INT_MAX);
+    priority_queue<pii, vii, greater<pii>> pq;
+    dis[s] = 0;
+    pq.push({0, s});
+    while (!pq.empty()) {
+        int u = pq.top().S;
+        pq.pop();
+        for (auto &v : G[u]) {
+            if (dis[u] + v.S < dis[v.F]) {
+                dis[v.F] = dis[u] + v.S;
+                pq.push({dis[v.F], v.F});
+            }
+        }
+    }
+}
+
+
+//for unweighted graph
+
+vi dis;
+
+void dijkstra(int s, int n)
+{
+    dis.assign(n, INT_MAX);
+    priority_queue<pii, vii, greater<pii>> pq;
+    dis[s] = 0;
+    pq.push({0, s});
+    while (!pq.empty()) {
+        int u = pq.top().S;
+        pq.pop();
+        for (auto &v : G[u]) {
+            if (dis[u] + 1 < dis[v]) {
+                dis[v] = dis[u] + 1;
+                pq.push({dis[v], v});
+            }
+        }
+    }
+}
