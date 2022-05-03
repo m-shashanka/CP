a89cbbf23134004ddf4fc36a23458fa4e7fd1ff7
M Shashanka <56446258+Shaky-Adino@users.noreply.github.com>
2022-05-03 18:27:31 +0530

    Add files via upload

---
+++ DSU.cpp
@@ -0,0 +1,30 @@
+//Union by Rank and Path Compression
+
+int parent[N], rank[N];
+
+void makeSet(n) {
+	for1(i, n) {
+		parent[i] = i;
+		rank[i] = 0;
+	}
+}
+
+int findPar(int node) {
+	if (node == parent[node])
+		return node;
+	return parent[node] = findPar(parent[node]);
+}
+
+void union(int u, int v) {
+	u = findPar(u);
+	v = findPar(v);
+
+	if (rank[u] < rank[v])
+		parent[u] = v;
+	else if (rank[v] < rank[u])
+		parent[v] = u;
+	else {
+		parent[v] = u;
+		rank[u]++;
+	}
+}
