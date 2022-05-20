70b63e365e0ea29c99fa1e6f39f0f749aa27edf7
M Shashanka <56446258+Shaky-Adino@users.noreply.github.com>
2022-05-20 17:04:38 +0530

    Add files via upload

---
+++ BridgesInGraph.cpp
@@ -0,0 +1,20 @@
+//dp solution
+
+void dfs(int u, int p, vector<bool> &vis, vector<int> &dp, vector<int> &lvl) {
+	vis[u] = true;
+	if (p != -1)
+		lvl[u] = lvl[p] + 1;
+	dp[u] = lvl[u];
+	for (auto v : G[u]) {
+		if (v == p)
+			continue;
+		if (vis[v])
+			dp[u] = min(dp[u], lvl[v]);
+		else {
+			dfs(v, u, vis, dp, lvl);
+			dp[u] = min(dp[u], dp[v]);
+			if (dp[v] > lvl[u])
+				cout << "Bridge from " << u << " to " << v << endl;
+		}
+	}
+}
