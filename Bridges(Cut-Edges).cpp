dde22b3af5531e8fd5b1e75ac0fa001f2991e78d
M Shashanka <56446258+Shaky-Adino@users.noreply.github.com>
2022-05-21 20:09:33 +0530

    Update Bridges(Cut-Edges).cpp

--- Bridges(Cut-Edges).cpp
+++ Bridges(Cut-Edges).cpp
@@ -6,6 +6,7 @@
 void dfs(int u, int p, vector<bool> &vis, vector<int> &tin, vector<int> &low, int &timer) {
 	vis[u] = true;
 	tin[u] = low[u] = timer++;
+	
 	for (auto v : G[u]) {
 		if (v == p)
 			continue;
@@ -14,7 +15,7 @@ void dfs(int u, int p, vector<bool> &vis, vector<int> &tin, vector<int> &low, in
 		else {
 			dfs(v, u, vis, tin, low, timer);
 			if (low[v] > tin[u])
-				cout << "Bridge from " << u << " to " << v << endl;
+				cout << "Bridge between " << u << " and " << v << endl;
 			low[u] = min(low[u], low[v]);
 		}
 	}
@@ -29,6 +30,7 @@ void dfs(int u, int p, vector<bool> &vis, vector<int> &dp, vector<int> &lvl) {
 	if (p != -1)
 		lvl[u] = lvl[p] + 1;
 	dp[u] = lvl[u];
+	
 	for (auto v : G[u]) {
 		if (v == p)
 			continue;
@@ -38,7 +40,7 @@ void dfs(int u, int p, vector<bool> &vis, vector<int> &dp, vector<int> &lvl) {
 			dfs(v, u, vis, dp, lvl);
 			dp[u] = min(dp[u], dp[v]);
 			if (dp[v] > lvl[u])
-				cout << "Bridge from " << u << " to " << v << endl;
+				cout << "Bridge between " << u << " and " << v << endl;
 		}
 	}
 }
