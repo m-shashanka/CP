951c10deee054652a605561637c2a16b8404e2bc
M Shashanka <56446258+Shaky-Adino@users.noreply.github.com>
2022-09-01 23:47:48 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,11 +1,11 @@
 //Union by Rank and Path Compression
 
-int parent[N], rank[N];
+int parent[N], Rank[N];
 
-void makeSet(n) {
+void makeSet(int n) {
 	for1(i, n) {
 		parent[i] = i;
-		rank[i] = 0;
+		Rank[i] = 0;
 	}
 }
 
@@ -19,12 +19,12 @@ void Union(int u, int v) {
 	u = findPar(u);
 	v = findPar(v);
 
-	if (rank[u] < rank[v])
+	if (Rank[u] < Rank[v])
 		parent[u] = v;
-	else if (rank[v] < rank[u])
+	else if (Rank[v] < Rank[u])
 		parent[v] = u;
 	else {
 		parent[v] = u;
-		rank[u]++;
+		Rank[u]++;
 	}
 }
