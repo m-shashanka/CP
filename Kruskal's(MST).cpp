e08f40a78b68068b1a5843196e5bc9043db5dedd
M Shashanka <56446258+Shaky-Adino@users.noreply.github.com>
2022-09-01 23:46:58 +0530

    Update Kruskal's(MST).cpp

--- Kruskal's(MST).cpp
+++ Kruskal's(MST).cpp
@@ -9,12 +9,12 @@ struct Edge {
 	}
 };
 
-int parent[N], rank[N];
+int parent[N], Rank[N];
 
 void makeSet(int n) {
 	for1(i, n) {
 		parent[i] = i;
-		rank[i] = 0;
+		Rank[i] = 0;
 	}
 }
 
@@ -28,13 +28,13 @@ void Union(int u, int v) {
 	u = findPar(u);
 	v = findPar(v);
 
-	if (rank[u] < rank[v])
+	if (Rank[u] < Rank[v])
 		parent[u] = v;
-	else if (rank[v] < rank[u])
+	else if (Rank[v] < Rank[u])
 		parent[v] = u;
 	else {
 		parent[v] = u;
-		rank[u]++;
+		Rank[u]++;
 	}
 }
 
